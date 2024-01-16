c647318ae47903782372019a61e160bbffcb829b
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-01-16 22:54:28 +0000

    会員マイページの制作途中

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,9 +1,6 @@
 class Public::CustomersController < ApplicationController
-  # アクセス権限
-  # before_action :authenticate_customer!
   
   def show
-    # 顧客のマイページ　URL：/customers/my_page　
     @customer = current_customer
   end
 
