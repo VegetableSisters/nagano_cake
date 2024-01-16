2627a4cd697209245afd3b8aa533718998f6991d
EC2 Default User <ec2-user@ip-172-31-26-160.ap-northeast-1.compute.internal>
2024-01-16 13:07:26 +0000

    orderのコントローラ着手

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,12 +3,45 @@ class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
   
   def new
+    @order = Order.new
   end
 
   def confirm
+    @shipping_cost = 800
+    @selected_pay_method = params[:order][:pay_method]
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+  
+    # 商品合計額の計算
+    ary = []
+    @cart_items.each do |cart_item|
+      ary << cart_item.item.price * cart_item.quantity
+    end
+    @cart_items_price = ary.sum
+  
+    @total_price = @shipping_cost + @cart_items_price
+    @address_type = params[:order][:address_type]
+  
+    case @address_type
+    when "customer_address"
+      @selected_address = current_customer.post_code + " " + current_customer.address + " " + current_customer.last_name + current_member.last_name
+    when "registered_address"
+      unless params[:order][:registered_address_id] == ""
+        selected = Address.find(params[:order][:registered_address_id])
+        @selected_address = selected.postal_code + " " + selected.address + " " + selected.name
+      else
+        render :new
+      end
+    when "new_address"
+      unless params[:order][:new_postal_code] == "" && params[:order][:new_address] == "" && params[:order][:new_name] == ""
+        @selected_address = params[:order][:new_postal_code] + " " + params[:order][:new_address] + " " + params[:order][:new_name]
+      else
+        render :new
+      end
+    end
   end
 
   def thanks
+    #特に記述なし
   end
 
   def create
