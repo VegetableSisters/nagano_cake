2a040203a6b5d076819387336baccce1820d0897
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-01-16 00:45:01 +0000

    テーブル定義書よりモデル作成

---
+++ db/migrate/20240116002133_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      
+      # テーブル定義書[商品]より作成
+      t.integer :genre_id
+      t.string :name
+      t.text :introduction
+      t.integer :price
+      t.boolean :is_active, default: true
+
+      t.timestamps
+    end
+  end
+end
