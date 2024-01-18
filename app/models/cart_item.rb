9fa5eca783625e1894f2c14c9eb484a515800bbd
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-01-18 10:50:04 +0000

    マイページのレイアウト修正

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,13 +1,6 @@
 class CartItem < ApplicationRecord
- belongs_to :customer
-  belongs_to :item
-
-  def add_tax_price
-    (self.price * 1.10).round
-  end
-
-  def subtotal
-    self.item.add_tax_price * self.amount
-  end
-
-  end
+  
+  # アソシエーション
+  belongs_to :customer
+  
+end
