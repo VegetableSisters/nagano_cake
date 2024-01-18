5963dd9ff40d612cbc459ef3482a84df932d7697
keke0813 <149664866+keke0813@users.noreply.github.com>
2024-01-18 08:54:49 +0000

    顧客　商品：一覧・詳細機能実装（レイアウト未完）

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.active.page(params[:page]).per(5)
-    @item_count = Item.active.count
+    @items = Item.all.page(params[:page]).per(5)
+    @item_count = Item.count
   end
 
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,7 +1,8 @@
 class CartItem < ApplicationRecord
-  
+
   # アソシエーション
   belongs_to :customer
-  
-  
+  belongs_to :item
+
+
 end
