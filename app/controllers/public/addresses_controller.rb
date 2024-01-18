6a1fbf414a0d7cc0feefb1880c777bf878b57a9a
keke0813 <149664866+keke0813@users.noreply.github.com>
2024-01-18 11:07:17 +0000

    tyuukannma-zi

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,14 +1,19 @@
 class Public::AddressesController < ApplicationController
   # アクセス権限
   before_action :authenticate_customer!
-  
+
   def index
+    @addresses = Addresse.all
+    @addresse = Addresse.new
   end
 
   def edit
   end
 
   def create
+    addresse = Addresse.new(addresse_params)
+    addresse.save
+    redirect_to admin_genres_path
   end
 
   def update
