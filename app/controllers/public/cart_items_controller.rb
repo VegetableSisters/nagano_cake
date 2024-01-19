1b63670742f1586fd5bef838853b437b5aa1f0af
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-01-19 07:33:54 +0000

    管理ショッピングカートの作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,14 +1,24 @@
 class Public::CartItemsController < ApplicationController
+  
   def index
+    @cart_items = current_customer.cart_items.all
   end
 
   def update
+    @cart_items = CartItem.find(params[:id])
   end
 
   def destroy
+    @cart_items = CartItem.find(params[:id])
+    cart_item.destroy
+    @cart_items = CartItem.all
+      redirect_to cart_items_path
   end
 
   def destroy_all
+    @cart_items = CartItem.all
+    cart_items.destroy_all
+      redirect_to cart_items_path
   end
 
   def create
