451b7547bf6af68dae55b1bcc7865922f10c12d6
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-01-19 11:45:35 +0000

    カートの全て削除の修正途中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -9,15 +9,13 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy
-    @cart_items = CartItem.find(params[:id])
+    cart_item = CartItem.find(params[:id])
     cart_item.destroy
-    @cart_items = CartItem.all
     redirect_to cart_items_path
   end
 
   def destroy_all
-    @cart_items = CartItem.all
-    cart_items.destroy_all
+    current_customer.cart_items.destroy_all
     redirect_to cart_items_path
   end
 
