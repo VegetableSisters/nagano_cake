3fb4757214485ec1fa6e1a79851de4a30eb11475
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-01-19 11:08:13 +0000

    カート機能の実装完了

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -22,5 +22,23 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+  @cart_item = current_customer.cart_items.new(cart_item_params)
+
+  # 同じ商品がすでに存在するかしないか
+  existing_cart_item = current_customer.cart_items.find_by(item_id: cart_item_params[:item_id])
+  if existing_cart_item
+    # 存在する場合、数量を更新
+    existing_cart_item.amount += cart_item_params[:amount].to_i
+    existing_cart_item.save
+  else
+    # 新しい商品なのでカートに追加
+    @cart_item.save
+  end
+    redirect_to cart_items_path
   end
 end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
