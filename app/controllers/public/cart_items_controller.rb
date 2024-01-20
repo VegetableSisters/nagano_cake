c0ef0eb8c6268c42b4262e9686d6242ea67c04ca
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2024-01-20 01:59:30 +0000

    ゲストユーザーがカートに商品を追加しようとすると、リダイレクトでログイン画面に遷移する機能を追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,5 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!, only: [:create, :update, :destroy, :destroy_all]
   
   def index
     @cart_items = current_customer.cart_items.all
@@ -19,24 +20,30 @@ class Public::CartItemsController < ApplicationController
     redirect_to cart_items_path
   end
 
-  def create
-  @cart_item = current_customer.cart_items.new(cart_item_params)
+ def create
+    # ログインしているかどうかのチェック
+    unless customer_signed_in?
+      # ログインしていない場合、ログイン画面にリダイレクト
+      redirect_to new_customer_session_path
+      return
+    end
 
-  # 同じ商品がすでに存在するかしないか
-  existing_cart_item = current_customer.cart_items.find_by(item_id: cart_item_params[:item_id])
-  if existing_cart_item
-    # 存在する場合、数量を更新
-    existing_cart_item.amount += cart_item_params[:amount].to_i
-    existing_cart_item.save
-  else
-    # 新しい商品なのでカートに追加
-    @cart_item.save
-  end
-    redirect_to cart_items_path
-  end
-end
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+
+    # 同じ商品がすでにカート内に存在するかチェック
+    existing_cart_item = current_customer.cart_items.find_by(item_id: cart_item_params[:item_id])
+    if existing_cart_item
+      # すでに存在する場合、数量を更新
+      existing_cart_item.amount += cart_item_params[:amount].to_i
+      redirect_to cart_items_path if existing_cart_item.save
+    else
+      # カートに新しい商品を追加
+      redirect_to cart_items_path if @cart_item.save
+    end
+ end
 
   private
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
   end
+end
