3fe74508107e2efac1e2030051b1977d9599f1b9
EC2 Default User <ec2-user@ip-172-31-26-160.ap-northeast-1.compute.internal>
2024-01-24 10:32:08 +0000

    注文、製作ステータス搭載

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,20 +1,31 @@
 class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
-  
-  def update
-  @order_detail = OrderDetail.find(params[:id])
 
-  if @order_detail.update(order_detail_params)
-    puts @order_detail.inspect # 追加行
-    redirect_to admin_order_path(@order_detail.order), notice: "製作ステータスが更新されました。"
-  else
-    render :edit
+
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_detail.update(order_detail_params)
+
+
+    if @order_detail.making_status == "製作中"
+      @order.update(status: "製作中")
+    end
+
+    if @order.order_details.all?{|order_detail| order_detail.making_status == "製作完了" }
+      @order.update(status: "発送準備中" )
+    else
+      @order.update(status: "製作中")
+    end
+
+    flash[:notice] = "更新に成功しました。"
+    redirect_to admin_order_path(@order_detail.order.id)
   end
-end
 
   private
-
   def order_detail_params
-  params.require(:order_detail).permit(:shipping_status, :production_status) # shipping_statusの他にproduction_statusも追加
-end
+    params.require(:order_detail).permit(:making_status)
+  end
+
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,17 +1,10 @@
 class OrderDetail < ApplicationRecord
-  enum shipping_status: {
-    payment_pending: 0,
-    payment_confirmed: 1,
-    being_prepared: 2,
-    preparing_for_shipping: 3,
-    shipped: 4
-  }
   
-  enum production_status: {
-    not_started: 0,
-    waiting_for_production: 1,
-    in_production: 2,
-    production_completed: 3
+  enum making_status: {
+    着手不可: 0,
+    製作待ち: 1,
+    製作中: 2,
+    製作完了: 3
   }
   belongs_to :order
   belongs_to :item
